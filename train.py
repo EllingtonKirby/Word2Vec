b58082f643d02d96e5bca443bee68b7545d993b9
Ellington <ellingtonkirby31@gmail.com>
2023-11-01 16:21:48 +0100

    add test to end of train

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ def save_model(model, optimizer, embedding_dim, radius, ratio, batch, epoch):
   )
   print(f'Model saved to {path}')
 
-def train_batched(model, optimizer, num_epochs, train_dataloader):
+def train_batched(model, optimizer, num_epochs, train_dataloader, test_dataloader):
   criterion = nn.BCELoss()
   losses = []
   for i in range(num_epochs):
@@ -33,5 +33,17 @@ def train_batched(model, optimizer, num_epochs, train_dataloader):
     losses.append(epoch_loss)
     print('-'*100)
     print(f'Epoch {i} done. Loss: {epoch_loss}')
+  
+  print('-'*100)
+  print('Evaluating test:')
+  with torch.no_grad():
+    test_loss = 0
+    for _, batch in enumerate(tqdm(test_dataloader)):
+      pos_outs, neg_outs = model(batch['word_id'].to(DEVICE), batch['positive_context_ids'].to(DEVICE), batch['negative_context_ids'].to(DEVICE))
+      stacked_outs = torch.hstack((pos_outs, neg_outs))
+      stacked_reals = torch.hstack((torch.zeros_like(pos_outs), torch.ones_like(neg_outs)))
+      loss = criterion(stacked_outs, stacked_reals)
+      test_loss += loss
+  print(f'Test loss: {test_loss}')
   
   return losses
