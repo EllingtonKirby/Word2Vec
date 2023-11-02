96fc936ab6c70848fc9830547607488331fd885f
Ellington <ellingtonkirby31@gmail.com>
2023-11-02 17:24:52 +0100

    fix the loss function

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ def train_batched(model, optimizer, num_epochs, train_dataloader, test_dataloade
       optimizer.zero_grad()
       pos_outs, neg_outs = model(batch['word_id'].to(DEVICE), batch['positive_context_ids'].to(DEVICE), batch['negative_context_ids'].to(DEVICE))
       stacked_outs = torch.hstack((pos_outs, neg_outs))
-      stacked_reals = torch.hstack((torch.zeros_like(pos_outs), torch.ones_like(neg_outs)))
+      stacked_reals = torch.hstack((torch.ones_like(pos_outs), torch.zeros_like(neg_outs)))
       loss = criterion(stacked_outs, stacked_reals)
       loss.backward()
       optimizer.step()
